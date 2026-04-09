c88509a7881a5de2404018a5b36eeeed8cd33f90
Davula Tejaswi <167857053+TejaswiDavula@users.noreply.github.com>
2026-04-09 12:36:55 +0530

    Add files via upload

---
+++ Time_Converter.py
@@ -0,0 +1,8 @@
+minutes = int(input("Enter minutes: ")) 
+hours = minutes // 60
+mins = minutes % 60
+
+if hours > 0:
+    print(f"{hours} hrs{'s' if hours > 1 else ''} {mins} minutes")
+else:
+    print(f"{mins} minutes")
